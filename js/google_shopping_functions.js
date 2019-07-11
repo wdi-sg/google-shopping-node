55311bee61109b00cc12f210b215db134b653788
irockmysock <benny_chin89@hotmail.com>
2019-07-11 19:21:20 +0800

    completed base question

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,18 +1,111 @@
 // use require to get the json file data
-
+const products = require('../products.json');
+const package = require('../package.json')
 /*
  * example function called getItemsCount
  * input: accepts full item data
  * output: returns the length of the items array
  */
-function getItemsCount(itemData) {
-  return itemData.items.length;
-}
+
+// function getItemsCount(itemData) {
+//   return itemData.items.length;
+// }
 
 /*
  * Define and use your functions here
  */
 
-// output item count using the getItemsCount function
-var data = {}; // replace this line with the data from the require statement
-console.log('Item Count: ' + getItemsCount(data));
+//Part 1
+function getItems () {
+    return products.items;
+    // console.log(products.items.length);
+    // console.log(products.items[2].product.inventories[0].availability === 'inStock');
+}
+// getItems();
+
+
+//Part 2
+function getItemsByBrand (item, brand) {
+    var brandArray = [];
+    for (i=0; i<item.length; i++) {
+        if (item[i].product.brand == brand) {
+            brandArray.push(item[i]);
+        };
+    }
+    console.log(`number of items by ${brand} is ${brandArray.length}`)
+    return brandArray;
+}
+
+// getItemsByBrand('Nikon');
+
+// Part 3
+function getItemsByAuthor(item, author) {
+    var itemsByAuthorArray =[];
+    for (i=0; i<item.length; i++) {
+        if (item[i].product.author.name.includes(author)) {
+            itemsByAuthorArray.push(item[i]);
+        };
+    }
+    console.log(`number of items by ${author} is ${itemsByAuthorArray.length}`);
+    return itemsByAuthorArray;
+};
+
+// getItemsByAuthor('eBay');
+
+//Part 4
+function getAvailableProducts (item) {
+    var availableProductArray = [];
+    for (i=0; i<item.length; i++) {
+        if (item[i].product.inventories[0].availability === 'inStock') {
+            availableProductArray.push(item[i]);
+        }
+    }
+    console.log(`available product count of item is: ${availableProductArray.length}`);
+    return availableProductArray;
+}
+
+// getAvailableProducts();
+
+
+//Part 5
+var items = getItems();
+
+// //All items made by Sony.
+var sonyItems = getItemsByBrand(items, 'Sony');
+console.log("items by sony listed below");
+console.log(sonyItems);
+console.log("End of Sony items array");
+
+// //All items made by Sony that are available.
+var availableSonyItems = getAvailableProducts(sonyItems);
+console.log("available sony items listed below");
+console.log(availableSonyItems);
+console.log("End of available Sony items array");
+
+//All available items by the author "Adorama Camera"
+var adoramaCameraItems = getItemsByAuthor(items, 'Adorama Camera');
+// console.log("items by author Adorama Camera listed below");
+// console.log(adoramaCameraItems);
+
+var availableAdorama = getAvailableProducts(adoramaCameraItems);
+console.log("available items by author Adorama Camera listed below");
+console.log(availableAdorama);
+console.log("End of available Adorama items array");
+
+
+//All items made by Nikon.
+var nikonItems = getItemsByBrand(items, 'Nikon');
+// console.log("items by Nikon listed below");
+// console.log(nikonItems);
+// All items made by Nikon with the author eBay.
+var nikonEbay = getItemsByAuthor(nikonItems,'eBay');
+console.log("nikon items by eBay listed below");
+console.log(nikonEbay);
+console.log("End of nikon by ebay items array");
+
+
+
+
+// // output item count using the getItemsCount function
+// var data = {}; // replace this line with the data from the require statement
+// console.log('Item Count: ' + getItemsCount(data));
