732ec2b7f99cc0e5de3b7659b02612f7aa626832
marcus <marcusteo@hotmail.com>
2019-07-11 22:28:26 +0800

    base work of assignment completed

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,18 +1,110 @@
 // use require to get the json file data
+var data = require("./../products")
+// console.log(data);
 
-/*
- * example function called getItemsCount
- * input: accepts full item data
- * output: returns the length of the items array
- */
-function getItemsCount(itemData) {
-  return itemData.items.length;
+//returns length of array
+var num = data.items.length;
+// console.log(data.items[0].product.inventories[0].availability)
+
+//to get item data from products.json
+var getData = function(data){
+  return data.items;
 }
+var googleProductData = getData(data)
+console.log(googleProductData);
+console.log(googleProductData.length);
 
-/*
- * Define and use your functions here
- */
+//getting items by brand
+var getItemsByBrand = function (googleProductData, input){
+  var arrayList = [];
+    for (var i = 0; i < googleProductData.length; i++){
+      if (googleProductData[i].product.brand === input){
+         arrayList.push (googleProductData[i].product.title);
+      }
+    }
+  return arrayList;
+}
+console.log("Items by Sony: "+ getItemsByBrand(googleProductData, "Sony"))
 
-// output item count using the getItemsCount function
-var data = {}; // replace this line with the data from the require statement
-console.log('Item Count: ' + getItemsCount(data));
+//getting items by author
+var getItemsByAuthor = function (googleProductData,input){
+  var arrayList = [];
+    for (var i = 0; i < googleProductData.length; i++){
+      if (googleProductData[i].product.author.name.includes(input)){
+         arrayList.push (googleProductData[i].product.title);
+      }
+    }
+  return arrayList;
+}
+console.log("Items sold by eBay: " + getItemsByAuthor(googleProductData, "eBay"));
+console.log("Items sold by eBay: " + getItemsByAuthor(googleProductData, "Nikon"));
+
+//getting items by availability sorted by author or brand
+var getAvailableProducts = function (input){
+  var arrayList = [];
+  for (var i = 0; i < data.items.length; i++){
+    if ((googleProductData[i].product.inventories[0].availability === "inStock" && googleProductData[i].product.author.name.includes(input)) || (googleProductData[i].product.inventories[0].availability === "inStock" && googleProductData[i].product.brand === input)){
+      arrayList.push (data.items[i].product.title);
+    }
+  }
+  return arrayList;
+}
+//nothing will show because adorama camera item is on backorder
+console.log("Available items by Adorama Camera: " + getAvailableProducts("Adorama Camera"));
+console.log("Available items by Sony: " + getAvailableProducts("Sony"));
+
+//using ES6 syntax arrow functions
+console.log("Items made by Nikon with the author eBay: " + getItemsByBrand(googleProductData, "Nikon").filter(item => getItemsByAuthor(googleProductData, "eBay").includes(item)));
+//doing it old school
+console.log("Items made by Nikon with the author eBay: " + getItemsByBrand(googleProductData, "Nikon").filter(function (item) {
+  return getItemsByAuthor(googleProductData, "eBay").includes(item)
+}));
+
+
+
+
+
+// var searchFunctions = {
+//   getItems: function(data){
+//     return data;
+//   },
+//   getItemsByBrand: function(input){
+//     var arrayList = [];
+//     for (var i = 0; i < data.items.length; i++){
+//       if (data.items[i].product.brand === input){
+//          arrayList.push (data.items[i].product.title);
+//       }
+//     }
+//     return arrayList;
+//   },
+//   getItemsByAuthor: function(input){
+//     var arrayList = [];
+//     for (var i = 0; i < data.items.length; i++){
+//       if (data.items[i].product.author.name.includes(input)){
+//         arrayList.push (data.items[i].product.title);
+//       }
+//     }
+//     return arrayList;
+//   },
+//   getAvailableProducts: function(input){
+//     var arrayList = [];
+//     if (input === undefined){
+//       for (var i = 0; i < data.items.length; i++){
+//         if (data.items[i].product.inventories[0].availability === "inStock"){
+//           arrayList.push (data.items[i].product.title);
+//         }
+//       }
+//     }else {
+//       for (var i = 0; i < data.items.length; i++){
+//         if (data.items[i].product.brand === input && data.items[i].product.inventories[0].availability === "inStock"){
+//           arrayList.push (data.items[i].product.title);
+//         }
+//       }
+//     }
+//     return arrayList;
+//   }
+// }
+
+// if (process.argv[2] || process.argv[3]){
+  // console.log(searchFunctions[process.argv[2]] (process.argv[3]) );
+// }
