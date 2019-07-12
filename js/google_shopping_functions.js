4176c0331d66f4a313d16878526d4a41fae825cf
Janet <cashhpvn@yahoo.com>
2019-07-12 10:22:54 +0800

    update

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,18 +1,54 @@
 // use require to get the json file data
+const data = require("../products.json");
+const items = getItems(data);
 
-/*
- * example function called getItemsCount
- * input: accepts full item data
- * output: returns the length of the items array
- */
-function getItemsCount(itemData) {
+ // * example function called getItemsCount
+ // * input: accepts full item data
+ // * output: returns the length of the items array
+
+var getItemsCount =function (itemData) {
   return itemData.items.length;
 }
+// console.log(getItemsCount(data))
 
-/*
- * Define and use your functions here
- */
+ function getItems(itemData){
+ 	return itemData.items;
+ }	
+ 
+function getItemsByBrand (items, brand){
+ 	 items.forEach(function(element){
+		 if(element.product.brand === brand){
+		 	console.log (element.product.title);
+		 }
+	});
+ }
+ 	 
+// getItemsByBrand(items,"Samsung")
 
-// output item count using the getItemsCount function
-var data = {}; // replace this line with the data from the require statement
-console.log('Item Count: ' + getItemsCount(data));
+function getItemsByAuthor(items, author){
+	items.forEach(function(element) {
+		if(element.product.author.name.includes(author)){
+			console.log(element.product.title)
+		}
+	});
+}
+
+// getItemsByAuthor(items,"pictureline.com")
+
+
+function getAvailableProducts(items){
+	items.forEach(function(element) {
+		if(element.product.inventories[0].availability === "inStock"){
+			console.log(element.product.title)
+		}
+	});
+}
+
+// getAvailableProducts(items)
+// console.log(process.argv);
+
+
+var brand = process.argv[2];
+console.log(brand);
+var brandItems = getItemsByBrand(items,brand);
+// console.log(brandItems)
