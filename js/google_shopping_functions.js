c120b79cf29e567768627757e46953b262913e7e
AQILAHRAJAB <AQILAH@MAJULAH.SG>
2019-07-13 21:44:16 +0800

    Basic without further

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,18 +1,70 @@
-// use require to get the json file data
+console.log("javascript works")
 
-/*
- * example function called getItemsCount
- * input: accepts full item data
- * output: returns the length of the items array
- */
-function getItemsCount(itemData) {
-  return itemData.items.length;
+//This function returns the items array from the google product object
+//Retrieve data from database
+function getItems(itemData) {
+    return itemData.items;
 }
 
-/*
- * Define and use your functions here
- */
+//create a function to retrieve the items by brand name
+function getItemsByBrand (items, brand) {
+    var brandArray = [];
+    for (var i = 0; i <items.length; i++) {
+        var brandsListed = itemsArray[i]["product"]["brand"];
+
+        if (brandsListed === brand) {
+            brandArray.push(itemsArray[i]);
+        }
+    }
+return(brandArray);
+
+}
+
+//create a function to retrieve the items by author name
+
+function getItemsByAuthor(items, author) {
+    var authorArray = [];
+    for (var i = 0; i <itemsArray.length; i++) {
+        var authorListed = itemsArray[i]["product"]["author"]["name"];
+
+        if (authorListed === author) {
+            authorArray.push([itemsArray[i]);
+        }
+    }
+return(authorArray);
+}
+
+//create a function to retrieve items by products
+
+function getItemsByProducts (items, products) {
+
+    var productsArray =[];
+     for (var i = 0; i <items.length; i++) {
+        var productsListed = itemsArray[i]["product"]["inventories"];
+
+    for (var j = 0; j <itemsArray.length; j++) {
+        var availability = inventories[j]["availability"];
+
+        if (availability === "inStock") {
+            productsArray.push(itemsArray[i]["product"]);
+        }
+    }
+}
+
+return(productsArray);
+}
+
+
+//using console.log retrieve the following items:
+//1. items made by Sony
+//2. items made by Sony that are available
+//3. items made by author "Adorama Camera"
+//4. items made by Nikon with the author eBay
+
 
 // output item count using the getItemsCount function
-var data = {}; // replace this line with the data from the require statement
+
+var itemsDatabase = require('../products.json');
+
+ // replace this line with the data from the require statement
 console.log('Item Count: ' + getItemsCount(data));
